14da37ef1dd2d814da7a5b534013a5e608265191
YounBrassebin <118263730+YounBrassebin@users.noreply.github.com>
2022-12-13 12:10:16 +0100

    Update Youn.cs

--- Adressage IP/Youn.cs
+++ Adressage IP/Youn.cs
@@ -6,7 +6,21 @@ using System.Threading.Tasks;
 
 namespace Adressage_IP
 {
-    class Youn
+  class Youn
     {
+        public static int[] BinDeci(int[,] binIP)
+        {
+            int[] ipDec = new int[4];
+
+
+
+            for (int octet = 0; octet < 4; octet++) 
+            {
+                for (int bit =7; bit >= 0; bit--)
+                {
+                    ipDec[octet] = ipDec[octet] + (int)Math.Pow(2, bit);
+                }
+            }
+            return ipDec;
+        }
     }
-}
