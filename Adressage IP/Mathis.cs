9c91693cab2fca03337f042a9c7dd5b786a7f68e
MaTmapoire <118263694+MaTmapoire@users.noreply.github.com>
2022-12-13 12:10:46 +0100

    CalculMasque

--- Adressage IP/Mathis.cs
+++ Adressage IP/Mathis.cs
@@ -4,9 +4,34 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Adressage_IP
+namespace iptruc
 {
     class Mathis
     {
+        public static int[,] CalculMasque(int CIDR)
+        {
+            int[,] masque = new int[4,8];
+
+            for (int octet = 0; octet < 4; octet++)
+                    
+
+                for (int bit = 7; bit >= 0; bit--)
+                    {
+                         if (CIDR>0)
+                    {
+                        masque[octet, bit] = 1;
+                        CIDR=CIDR-1;
+                    }
+                    else
+                    {
+                        masque[octet, bit] = 0;
+                    }
+                    }
+
+            }
+
+            
+        }
+        
     }
 }
