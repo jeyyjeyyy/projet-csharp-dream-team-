f6d3d3316303545c330d89a624545364076897dd
BlanchardGwenael <118263834+BlanchardGwenael@users.noreply.github.com>
2022-12-13 12:04:58 +0100

    DecimalBinaire

--- Adressage IP/Gwenael.cs
+++ Adressage IP/Gwenael.cs
@@ -2,11 +2,31 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
-namespace Adressage_IP
+namespace AppAdresseIP
 {
-    class Gwenael
+    public class Gwenael
     {
+        public static int[,] DecimalBinaire(int[] ipDecimal)
+        {
+            int[,] resultat = new int[4, 8];
+            foreach(int indice in ipDecimal)
+            {
+                int tmp = indice;
+                for(int bit = 7; bit >= 0; bit--)
+                {
+                    if(tmp >= Math.Pow(2, bit))
+                    {
+                        resultat[Array.IndexOf(ipDecimal, indice), bit] = 1;
+                        tmp = (int)(tmp - Math.Pow(2, bit));
+                    }
+                    else
+                    {
+                        resultat[Array.IndexOf(ipDecimal, indice), bit] = 0;
+                    }
+                }
+            }
+            return resultat;
+        }
     }
 }
